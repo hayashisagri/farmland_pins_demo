ececa655ff82826b1e1737c93798101c5e663422
hayashisagri <113956610+hayashisagri@users.noreply.github.com>
2022-11-21 17:17:48 +0900

    fix script

--- japan/models.py
+++ japan/models.py
@@ -5,14 +5,11 @@ class JapanPolygon(models.Model):
     land_id = models.CharField('id', max_length=50)
     mpoly = models.MultiPolygonField(srid=4326)
 
-# class FarmlandPins(models.Model):
-
 
 class FarmlandPinOriginals(models.Model):
-
     city_code = models.CharField(max_length=10)
-    latitude = models.DecimalField(max_digits=20, decimal_places=17)
-    longitude = models.DecimalField(max_digits=20, decimal_places=17)
+    point = models.PointField(geography=True, srid=4326)
+    prefecture = models.CharField(max_length=5)
     address = models.CharField(max_length=100)
 
     created_at = models.DateTimeField(auto_now_add=True)
--- japan/views.py
+++ japan/views.py
@@ -1,144 +1,227 @@
-import unicodedata
-import requests
-from japan.data import SAPPORO_POLYGONS
-from django.contrib.gis.geos import Point
+import logging
 
-from django.shortcuts import render, redirect
+import requests
+
+from django.contrib.gis.geos import GEOSGeometry
 
 from japan.models import JapanPolygon, FarmlandPinOriginals
 
+LOGGER = logging.getLogger('django')
 
-def root_view(request):
-    return redirect('map')
+PREFECTURES_MASTER = (
+    ('北海道', '01'),
+    ('青森県', '02'),
+    ('岩手県', '03'),
+    ('宮城県', '04'),
+    ('秋田県', '05'),
+    ('山形県', '06'),
+    ('福島県', '07'),
+    ('茨城県', '08'),
+    ('栃木県', '09'),
+    ('群馬県', '10'),
+    ('埼玉県', '11'),
+    ('千葉県', '12'),
+    ('東京都', '13'),
+    ('神奈川県', '14'),
+    ('新潟県', '15'),
+    ('富山県', '16'),
+    ('石川県', '17'),
+    ('福井県', '18'),
+    ('山梨県', '19'),
+    ('長野県', '20'),
+    ('岐阜県', '21'),
+    ('静岡県', '22'),
+    ('愛知県', '23'),
+    ('三重県', '24'),
+    ('滋賀県', '25'),
+    ('京都府', '26'),
+    ('大阪府', '27'),
+    ('兵庫県', '28'),
+    ('奈良県', '29'),
+    ('和歌山県', '30'),
+    ('鳥取県', '31'),
+    ('島根県', '32'),
+    ('岡山県', '33'),
+    ('広島県', '34'),
+    ('山口県', '35'),
+    ('徳島県', '36'),
+    ('香川県', '37'),
+    ('愛媛県', '38'),
+    ('高知県', '39'),
+    ('福岡県', '40'),
+    ('佐賀県', '41'),
+    ('長崎県', '42'),
+    ('熊本県', '43'),
+    ('大分県', '44'),
+    ('宮崎県', '45'),
+    ('鹿児島県', '46'),
+    ('沖縄県', '47')
+)
 
 
-def map_view(request):
-    if request.POST:
-        farmland_pin_address = request.POST.get('farmland_pin_address')
-        cleaned_input_address = unicodedata.normalize("NFKC", farmland_pin_address)
-        farmland_pins = SAPPORO_POLYGONS
+class Wagri:
+    AUTH_URL = 'https://api.wagri.net/Token'
+    WAGRI_ID = 'grant_type=client_credentials&client_id=fcb04d4d-72da-4965-8076-f655ebb7f966&client_secret=SAgri2018'
+    WAGRI_URL = "https://api.wagri.net/API/Public/AgriculturalLand/SearchByCityCode?CityCode=011002"
+
+    def get_wagri_auth_token(self) -> str:
         """
-        農地ピンデータについては暫定で、data.pyにそのまま保存をしている。
-        入力された地番（cleaned_input_address）を使用して、data.pyのデータを検索し、
-        一致した地番のオブジェクトから緯度、経度を取得している。
-        実際にはRDBにデータは保管予定なので修正の必要あり。
-
-        緯度経度の順番に注意、containsで検索できなくなる
-
-        農地ピンの測地系 JGD2000(?) SRID4612
-        筆ポリゴンの測地系 JGD2011 SRID6668
-
-        1. 検索部分を引数で受け取り、該当するポリゴンを戻値で返す
-        2. 都道府県と市区町村（オプション）、retry可能にする（再検索）
+        :return: 会員情報に基づきapi認証用のトークンを返却する（有効期限12時間）
         """
-        long, lat = 0.0, 0.0
-        for fp in range(len(farmland_pins)):
-            if cleaned_input_address in farmland_pins[fp].values():
-                pin_dict = farmland_pins[fp]
-                long = pin_dict['Longitude']
-                lat = pin_dict['Latitude']
+        auth_token = requests.post(self.AUTH_URL, self.WAGRI_ID).json()['access_token']
+        return auth_token
 
-        pnt = Point(long, lat)
+    def get_farmland_pins_by_city_code(self, city_code: str, auth_token: str) -> str:
+        """
+        :param city_code: 6桁の市区町村コード
+        :param auth_token: wagriの認証トークン
+        :return: 指定の市区町村の農地ピン情報を返却
+        """
+        headers = {'X-Authorization': auth_token}
+        farmland_pins = requests.get(self.WAGRI_URL, headers=headers).json()
+        return farmland_pins
+
+    def load_farmland_pins(self, auth_token: str, city_code: str):
+        """
+        :param auth_token: wagriの認証トークン
+        :param city_code: 6桁の市区町村コード
+        :return:
+        """
+        farmland_pins = Wagri.get_farmland_pins_by_city_code(city_code, auth_token)
+        objs = []
+        for pin in farmland_pins:
+            prefecture, address = Wagri.strip_prefecture_from_pin_address(pin)
+            pnt = GEOSGeometry(f"POINT({pin['Longitude']} {pin['Latitude']})")
+            obj = FarmlandPinOriginals(
+                city_code=pin['CityCode'],
+                prefecture=prefecture,
+                address=address,
+                point=pnt,
+            )
+            objs.append(obj)
+
+        FarmlandPinOriginals.objects.bulk_create(objs)
+
+    def strip_prefecture_from_pin_address(self, pin: dict):
+        """
+        :param pin: wagriから取得した農地ピン情報
+        :return: 農地ピン情報のaddressから都道府県名とそれ以外を分けて返却
+        """
+        prefecture_code = pin['CityCode'][:2]
+        prefecture = ''
+        original_address = pin['Address']
+        for p in PREFECTURES_MASTER:
+            if prefecture_code == p[1]:
+                prefecture = p[0]
+        address = original_address.removeprefix(prefecture)
+        return prefecture, address
+
+
+class FarmlandPins:
+
+    def search_farmland_pins(self, prefecture: str, address: str) -> list | None:
+        """
+        :param prefecture: 都道府県名
+        :param address: 住所（地番）
+        :return: あいまい検索でヒットした農地ピンのリストを返却
+        """
         try:
-            multi_polygon = JapanPolygon.objects.get(mpoly__contains=pnt).mpoly.json
+            pins = FarmlandPinOriginals.objects.filter(prefecture__contains=prefecture, address__contains=address)
+            return pins
         except:
-            multi_polygon = {"type": "MultiPolygon", "coordinates": [[[[0, 0], [0, 0], [0, 0]]]]}
-            print('error')
+            message = f"unexpected input value. value: {prefecture, address}"
+            LOGGER.error(message)
+            return None
 
-        return render(
-            request,
-            'japan/map.html',
-            {
-                'lat': lat,
-                'long': long,
-                'address': f'"{cleaned_input_address}"',
-                'coordinates': multi_polygon,
-            }
-        )
-
-    return render(
-        request,
-        'japan/map.html'
-    )
-
-
-# wagriの会員IDとパスワードからapi用の認証トークンを取得
-def get_wagri_auth_token():
-    auth_url = 'https://api.wagri.net/Token'
-    wagri_id = 'askme'
-    auth_token = requests.post(auth_url, wagri_id).json()['access_token']
-    return auth_token
-
-
-# 市町村コードから農地ピンを取得する
-def get_farmland_pins_by_city_code(city_code, auth_token):
-    wagri_url = f"https://api.wagri.net/API/Public/AgriculturalLand/SearchByCityCode?CityCode={city_code}"
-    headers = {'X-Authorization': auth_token}
-    farmland_pins = requests.get(wagri_url, headers=headers).json()
-    return farmland_pins
-
-
-# wagriから取得した農地ピンをマスターテーブルに保存する
-# shellで中身を一行ずつ読み込んだら動くのに関数を呼んだらエラーになる、なぜ。。。pin = farmland_pins[i]
-# TypeError: 'list' object is not callable
-def load_farmland_pins():
-    # auth_token = get_wagri_auth_token()
-    # city_code = 000000
-    farmland_pins = SAPPORO_POLYGONS
-    objs = []
-    for i in range(len(farmland_pins)):
-        pin = farmland_pins[i]
-        obj = FarmlandPinOriginals(
-            city_code=pin['CityCode'],
-            latitude=pin['Latitude'],
-            longitude=pin['Longitude'],
-            address=pin['Address'],
-        )
-        objs.append(obj)
-
-    FarmlandPinOriginals.objects.bulk_create(objs)
-
-
-# 住所などから農地ピンを取得する
-def get_farmland_pins_by_address(prefecture, city, address):
-    if address:
+    def search_farmland_pin(self, address: str):
+        """
+        :param address: 住所（地番）
+        :return: 完全一致する農地ピンを返却
+        """
         try:
-            pin = FarmlandPinOriginals.objects.filter(address='北海道札幌市北区新川743-3')
+            pin = FarmlandPinOriginals.objects.get(address=address)
             return pin
         except:
-            print('invalid value is given')
-    elif city:
-        try:
-            pins = FarmlandPinOriginals.objects.filter(address__contains=city)
-            return pins
-        except:
-            print('invalid value is given')
-    elif prefecture:
-        try:
-            pins = FarmlandPinOriginals.objects.filter(address__contains=prefecture)
-            return pins
-        except:
-            print('invalid value is given')
-    raise  # 例外処理
+            message = f"unexpected input value. value: {address}"
+            LOGGER.error(message)
+            return None
+
+    def get_polygons_by_farmland_pins(self, pins: list) -> list:
+        """
+        :param pins: 農地ピンのリスト
+        :return: 農地ピンと重なるポリゴンのリスト
+        """
+        polygons = []
+        for pin in pins:
+            try:
+                polygon = JapanPolygon.objects.get(mpoly__contains=pin.point).mpoly.json
+                polygons.append(polygon)
+            except:
+                message = f"matching polygon does not exist. farmland_pin_originals id:{pin.id} address:{pin.address}"
+                LOGGER.info(message)
+        return polygons
+
+    def get_polygon_by_farmland_pin(self, pin):
+        """
+        :param pin: 単体の農地ピン情報
+        :return: 農地ピンと重なる単体のポリゴン
+        """
+        polygon = JapanPolygon.objects.get(mpoly__contains=pin.point).mpoly.json
+        return polygon
+
+    def get_pins_by_distance(self, polygon: str, distance: float):
+        """
+        :param polygon: ポリゴン
+        :param distance: ポリゴンの中心からの距離（半径）メートル
+        :return: 距離以内の農地ピンリスト
+        """
+        distance_limit = 5000  # todo: どこまでの距離を検索対象として許可するか要検討
+        if distance >= distance_limit:
+            message = f"distance is too large. must be smaller than {distance_limit}"
+            LOGGER.error(message)
+            return None
+        pnt_center = GEOSGeometry(polygon).centroid
+        pins = FarmlandPinOriginals.objects.filter(point__dwithin=(pnt_center, distance))
+        return pins
 
 
-# 農地ピンのリストをキーに、ポリゴンのリストを取得する
-def get_polygons_by_farmland_pins():
-    search_input = 'sample address'  # 検索ワード、都道府県名や住所など
-    pins = get_farmland_pins_by_address(prefecture='', city='', address=search_input)
-    polygons = []
-    for pin in pins:
-        polygon = get_polygon_by_farmland_pin(pin)
-        polygons.append(polygon)
-    return polygons
+# 農地ピンのサンプル（不要な要素は落としています）
+pin_sample = [
+    {
+        "CityCode": "462136",
+        "Latitude": 30.7009760660001,
+        "Longitude": 131.065587658,
+        "Address": "北海道札幌市中央区盤渓279-4",
+    },
+    {
+        "CityCode": "462136",
+        "Latitude": 30.6804379820001,
+        "Longitude": 131.051926976,
+        "Address": "北海道札幌市中央区盤渓279-8",
+    }
+]
 
+# メソッド呼び出しサンプル
+Wagri.get_wagri_auth_token()
+Wagri.get_farmland_pins_by_city_code('010101', 'トークン')
+Wagri.load_farmland_pins('010101', 'トークン')
+FarmlandPins.strip_prefecture_from_pin_address(pin_sample[0])
+# prefecture='北海道', address='札幌市中央区盤渓279-4'
 
-# 個別の農地ピンをキーに個別のポリゴンを取得する
-def get_polygon_by_farmland_pin(pin):
-    # Decimal('43.04352666500010116')
-    # 緯度経度のみを取り出して、座標として設定する方法がわからない
-    # pnt = Point(pnt.longitude, pnt.latitude)
-    # TypeError: Invalid parameters given for Point initialization.
-    pnt = Point(pin.longitude, pin.latitude)
-    polygon = JapanPolygon.objects.get(mpoly__contains=pnt).mpoly.json
-    return polygon
+FarmlandPins.search_farmland_pins(prefecture='北海', address='盤渓')
+# [<FarmlandPinOriginals: FarmlandPinOriginals object (1)>, <FarmlandPinOriginals: FarmlandPinOriginals object (2)>, ....]
+
+FarmlandPins.search_farmland_pin(address='札幌市中央区盤渓279-4')
+# [<FarmlandPinOriginals: FarmlandPinOriginals object (1)>]
+
+FarmlandPins.get_polygons_by_farmland_pins(
+    pins='[<FarmlandPinOriginals: FarmlandPinOriginals object (1)>, <FarmlandPinOriginals: FarmlandPinOriginals object (2)>, .....]')
+# ['{ "type": "MultiPolygon", "coordinates": [ [ [ [ 141.258993790830999, 43.043309390616102 ], [ 141.258974796249987, 43.043334142656697 ], [ 141.258902917036011, 43.043398084454203 ] ] ] ] }']
+
+FarmlandPins.get_polygon_by_farmland_pin(pin='<FarmlandPinOriginals: FarmlandPinOriginals object (1)>')
+# '{ "type": "MultiPolygon", "coordinates": [ [ [ [ 141.258993790830999, 43.043309390616102 ], [ 141.258974796249987, 43.043334142656697 ], [ 141.258902917036011, 43.043398084454203 ]......}'
+
+FarmlandPins.get_pins_by_distance(
+    polygon='{ "type": "MultiPolygon", "coordinates": [ [ [ [ 141.258993790830999, 43.043309390616102 ]....',
+    distance=1000)
+# [<FarmlandPinOriginals: FarmlandPinOriginals object (1)>, <FarmlandPinOriginals: FarmlandPinOriginals object (2)>, ....]
